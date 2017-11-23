6bb979e8fc1b74803fbb2c00960f4394a7de8730
Harrison Baxter <bhazington@gmail.com>
2017-11-23 14:46:46 +0000

    Great library upgrade

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -13,12 +13,8 @@ object MicroServiceBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.13.0",
-    "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
-    "uk.gov.hmrc" %% "play-health" % "2.1.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "6.13.0",
     "uk.gov.hmrc" %% "play-url-binders" % "2.1.0",
-    "uk.gov.hmrc" %% "play-config" % "4.2.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "uk.gov.hmrc" %% "domain" % "4.1.0"
   )
 
